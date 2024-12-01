57c4b4a3930f3be1a92dad9a287b027b69f81a36
HobbesUnleashed <sleacy@live.co.uk>
2024-12-01 14:06:39 +0000

    Function to introduce the game and begin requesting user interaction added

--- run.py
+++ run.py
@@ -88,3 +88,41 @@ hangman_art = [
     =========
     You have been hanged!'''
 ]
+
+def intro():
+    '''
+    Provide a welcome screen for the user.
+    '''
+    print("Welcome to\n")
+    #Print the games introduction art
+    print(intro_art)
+    #Print a blurb to the game
+    print("\nA game in which you guess the letters in a word in order to stay alive!!")
+    #Ask if the user would like to play
+    print("\nWould you like to play? (Y/N)")
+    #Receive the user input and convert it to uppercase
+    play = input("> ").upper()
+    #If the user selects Y for yes
+    if play == "Y":
+        #Print the following messages - including an option to see the rules
+        print("\nGreat! Let's play!")
+        print("\nWould you like to see the rules? (Y/N)")
+        #Receive the input regarding the rules and convert to uppercase
+        rules = input("> ").upper()
+        #If the user selects Y for yes
+        if rules == "Y":
+            #Run the help function which details the rules
+            help()
+        #Otherwise
+        else:
+            #Print this message and load the next function from main()
+            print("\nOkay, let's play!")
+    #If user selects N for no in relation to playing the game
+    elif play == "N":
+        #Print this message and then exit the runtime
+        print("\nOkay, maybe next time!")
+        exit()
+    #Otherwise - any other input will receive this message and the intro() function will begin again
+    else:
+        print("\nInvalid input. Please try again.\n")
+        intro()
